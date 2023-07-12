8c7283820b9983ae52ce4a87e00d0875488cfc92
zi-longh <Zi_long2020@yeah.net>
2023-07-12 17:15:54 +0800

    goods

--- src/main/resources/dbfiles/t_goods.sql
+++ src/main/resources/dbfiles/t_goods.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80029
  File Encoding         : 65001
 
- Date: 12/07/2023 10:32:47
+ Date: 12/07/2023 17:08:06
 */
 
 SET NAMES utf8mb4;
@@ -22,18 +22,18 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `t_goods`;
 CREATE TABLE `t_goods`  (
-  `goodsNum` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '商品编号',
+  `goodsNum` int NOT NULL AUTO_INCREMENT COMMENT '商品编号',
   `goodsName` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '商品名称',
   `goodsType` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '类别',
-  `goodsPrice` int NULL DEFAULT NULL COMMENT '售价',
-  `goodsCost` int NULL DEFAULT NULL COMMENT '成本',
+  `goodsPrice` double NULL DEFAULT NULL COMMENT '售价',
+  `goodsCost` double NULL DEFAULT NULL COMMENT '成本',
   PRIMARY KEY (`goodsNum`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_goods
 -- ----------------------------
-INSERT INTO `t_goods` VALUES ('1', '可乐', '饮料', 4, 3);
-INSERT INTO `t_goods` VALUES ('2', '纸巾', '日用品', 1, 2);
+INSERT INTO `t_goods` VALUES (1, '可乐', '饮料', 4, 3);
+INSERT INTO `t_goods` VALUES (2, '纸巾', '日用品', 1, 2);
 
 SET FOREIGN_KEY_CHECKS = 1;
